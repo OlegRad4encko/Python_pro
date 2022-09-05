6f6f80560d7a7d5862da756f0c55f43e1586d65b
Oleh Sergeevich <oleg.rad4encko@gmail.com>
2022-09-05 23:42:25 +0300

    Fix Homework 8

--- Lesson_8/ex_3.py
+++ Lesson_8/ex_3.py
@@ -1,18 +1,21 @@
 # Напишите функцию, которая применит к списку чисел произвольную
 # пользовательскую функцию и вернет суммы элементов полученного списка.
 
-def parent_func(ulist: list):
+def parent_func(ulist: list, ufunc):
     if not isinstance(ulist, list):
         raise TypeError("ulist must be a list")
 
-    def user_func(u_list):
-        for i in range(len(u_list)):
-            u_list[i] = (u_list[i] - 5) * 2
-
-    user_func(ulist)
+    ulist = ufunc(ulist)
     return sum(ulist)
+
+
+def user_func(u_list):
+    for i in range(len(u_list)):
+        u_list[i] = (u_list[i] - 5) * 2
+
+    return u_list
 
 
 uu_list = [2, 1, 5, 6, 34, 6, 3, -2, 3, 54, 2, 4]
 
-print(parent_func(uu_list))
+print(parent_func(uu_list, user_func))
